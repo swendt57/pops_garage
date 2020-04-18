905a047d7ad12a9e16af2630b9fb3885f3d9c76e
Steve Wendt <stevewendt@cox.net>
2020-04-17 18:38:52 -0700

    Finished Land Cruiser story. Needs gallery added. Also started working on deploying to Heroku.

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -9,6 +9,7 @@ import ClubNews from '../containers/ClubNews';
 import Library from '../containers/Library';
 import MetropolitanStory from '../containers/MetropolitanStory';
 import NashRambler from '../containers/NashRambler';
+import LandCruiser from "../containers/LandCruiser";
 
 // import NotFoundPage from '../components/NotFoundPage';
 
@@ -25,6 +26,7 @@ const Routes = () => {
             <Route path="/club_news" component={ClubNews}/>
             <Route path="/metropolitan_story" component={MetropolitanStory}/>
             <Route path="/nash_rambler" component={NashRambler}/>
+            <Route path="/land_cruiser" component={LandCruiser}/>
 
             {/*<Route component={NotFoundPage}/>*/}
         </Switch>
