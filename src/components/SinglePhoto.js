12c48d31d224214d921f877ffa0d5043f21af134
swendt57 <Lthr4ever>
2020-04-25 13:48:57 -0700

    Oops - one more gallery added for the Studebaker story. Also cleaned up other Tire Kicking entries a bit

--- src/components/SinglePhoto.js
+++ src/components/SinglePhoto.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SinglePhoto = props => {
 
-    return ( <div>
+    return ( <div className="single-photo-div">
             <img src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`}
                       className={props.class_name}
                       alt={props.alt_text + props.title} />
