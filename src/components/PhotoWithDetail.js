4e8b7092c8a30d59008359e9a4587db2c031b6d7
swendt57 <Lthr4ever>
2020-05-02 22:51:26 -0700

    Updated Photo with details component becasue the table was causing the image and text to appear above the navbar.

--- src/components/PhotoWithDetail.js
+++ src/components/PhotoWithDetail.js
@@ -2,22 +2,42 @@ import React from "react";
 
 const PhotoWithDetail = props => {
 
-    return ( <div className="table">
-                <div className="row">
-                    <div className="col"><img src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`}
-                                              className={props.class_name}
-                                              alt={props.alt_text + props.title} />
-                                            {/* if a URL is is provided, hyperlink the title else just show the title */}
-                                            {props.link ? <a href={window.location.origin + "/" + props.link}><strong>{props.title}</strong></a>
-                                                : <strong>{props.title}</strong>}
-                                                &mdash;{props.description}
+    return (
+        <React.Fragment>
+        <div className="photo-detail">
+        <p><img src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`} className={props.class_name} alt={props.alt_text + props.title}/>
 
-                    </div>
-                </div>
+        {props.link ? <a href={window.location.origin + "/" + props.link}><strong>{props.title}</strong></a>
+            : <strong>{props.title}</strong>}&mdash;{props.description}</p>
+        </div>
 
-                <hr/>
-            </div>
+          <hr/>
+        </React.Fragment>
     )
 }
 
 export default PhotoWithDetail
+
+{/*if a URL is is provided, hyperlink the title else just show the title*/}
+
+{/* <div> */}
+{/* <div className="" style={{ display: "flex"}}>*/}
+{/**/}
+{/*      <div style={{flex: 1, margin: "0 10px 5px"}}>*/}
+{/*          <img src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`}*/}
+{/*               className={props.class_name}*/}
+{/*               alt={props.alt_text + props.title}/>*/}
+{/*      </div>*/}
+{/**/}
+{/*      <div style={{flexGrow: 2, textAlign: "justify"}}>*/}
+{/*             /!* if a URL is is provided, hyperlink the title else just show the title *!/*/}
+{/*             {props.link ? <a href={window.location.origin + "/" + props.link}><strong>{props.title}</strong></a>*/}
+{/*                 : <strong>{props.title}</strong>}*/}
+{/*             &mdash;{props.description}*/}
+{/*     </div>*/}
+{/**/}
+{/**/}
+{/* </div>*/}
+{/**/}
+{/*<hr/>*/}
+{/*}</div>*/}
