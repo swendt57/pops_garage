d43036de100cd027e9e47437f2859f04cd67aba1
swendt57 <Lthr4ever>
2020-05-02 21:01:02 -0700

    Fixed most issues with the banner and navbar. I am commiting before I attempt minor tweaks

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,104 +1,67 @@
 import React from "react";
 
-import banner from '../assets/images/banner-new.jpg';
-import bg from '../assets/images/bg.jpg';
+import bg from '../assets/images/bg-extended.png';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import {Link} from 'react-router-dom';
 
+import $ from 'jquery';
+
 const HeaderNav = props => {
 
-    let beachDiv = {};
-
-    if (props.hideBanner) {
-        console.log("hiding");
-        beachDiv = {
-            backgroundImage: "url(" + bg + ")",
-            backgroundRepeat: "repeat",
-            backgroundPositionX: "0"
-        }
+    //TODO Research how to do this properly
+    if (props.toggleBanner) {
+        $(".navbar").toggleClass("beach-div");
     } else {
-        console.log("showing");
-        beachDiv = {
-            backgroundImage: "url(" + banner + "), url(" + bg + ")",
-            backgroundRepeat: "no-repeat, repeat",
-            backgroundPositionX: "119px, 0",
-        }
+        $(".navbar").toggleClass("beach-div");
     }
 
     let bgDiv = {
         backgroundImage: "url(" + bg + ")",
         backgroundRepeatX: "repeat",
         backgroundRepeatY: "no-repeat",
-        // backgroundPositionY: "1px",
-        // height: "100%",
-        // zIndex: "100",
         marginBottom: "20px",
         borderBottom: "2px solid black"
     };
 
     return (
         <header>
-            {/*  style={bgDiv} */}
-            <div id="header-bg" style={beachDiv}>
-            <div className="nav-container">
-                <div className="nav-wrapper">
-                    {/*  style={bgDiv} */}
-                    <div/>
-                    {/* backgroundColor: "#88bbda",  */}
-                    {/*using background here cuts off palm tree on wider screens*/}
-                    <Navbar sticky="top" collapseOnSelect expand="lg" style={{backgroundColor: "#88bbda"}} >
-                        {/*  style={beachDiv} */}
-                        <Navbar.Brand href="#home" className="logo-container">
+            <div id="header-bg" style={bgDiv}>
+                <div className="container">
+                    <Navbar sticky="top" collapseOnSelect bg="light" expand="lg" className="beach-div">
+                        <Navbar.Brand href="/">
                             <div className="logo-wrapper">
                                 <div className="title">Pop's Garage</div>
                                 <div className="subtitle">Beach Bum Cove, California</div>
                                 <div className="tag-line">"a bit north of Margaritaville"</div>
                             </div>
-                            {/*<img src={banner} style={{margin: 0}}/>*/}
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav" style={{}}>
-                            <Nav className="ml-auto text-right" style={{backgroundColor: "transparent"}}>
-                                <Nav.Link eventKey="1" as={Link} to="/" style={{color: "#181cb3"}}>
-                                    Home
-                                </Nav.Link>
-                                <Nav.Link eventKey="2" as={Link} to="showroom" style={{color: "#181cb3"}}>
-                                    Showroom
-                                </Nav.Link>
-                                <Link to="/" className="nav-link">Home</Link>
-                                <Link to="showroom" className="nav-link">Showroom</Link>
-                                <Link to="tire_kicking" className="nav-link">Tire Kicking</Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="text-right ">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="1" as={Link} to="/">Home</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="2" as={Link} to="showroom">Showroom</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="3" as={Link} to="tire_kicking">Tire Kicking</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="4" as={Link} to="livery">The Livery</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="5" as={Link} to="library">The Library</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="6" as={Link} to="club_news">Club News</Nav.Link>
+                                </Nav.Item>
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
-
-                    {/*<Navbar bg="dark" expand="lg" style={beachDiv}>*/}
-                    {/*    <Navbar.Brand href="#home" className="logo-container">*/}
-                    {/*        <div className="logo-wrapper">*/}
-                    {/*            <div className="title">Pop's Garage</div>*/}
-                    {/*            <div className="subtitle">Beach Bum Cove, California</div>*/}
-                    {/*            <div className="tag-line">"a bit north of Margaritaville"</div>*/}
-                    {/*        </div>*/}
-                    {/*    </Navbar.Brand>*/}
-
-                    {/*    <Navbar.Toggle aria-controls="basic-navbar-nav"/>*/}
-                    {/*    <Navbar.Collapse id="basic-navbar-nav">*/}
-
-                    {/*        <Nav className="text-right ml-auto">*/}
-                    {/*            <Link to="/" className="nav-link">Home</Link>*/}
-                    {/*            <Link to="showroom" className="nav-link">Showroom</Link>*/}
-                    {/*            <Link to="tire_kicking" className="nav-link">Tire Kicking</Link>*/}
-                    {/*            <Link to="livery" className="nav-link">The Livery</Link>*/}
-                    {/*            <Link to="library" className="nav-link">The Library</Link>*/}
-                    {/*            <Link to="club_news" className="nav-link">Club News</Link>*/}
-                    {/*        </Nav>*/}
-                    {/*    </Navbar.Collapse>*/}
-                    {/*</Navbar>*/}
                 </div>
-            </div>
             </div>
         </header>
     )
@@ -106,5 +69,3 @@ const HeaderNav = props => {
 
 
 export default HeaderNav;
-
-//style={beachDiv}
