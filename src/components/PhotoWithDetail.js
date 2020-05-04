58656d0375450a382517ac4902769711f5806bc9
Steve Wendt <stevewendt@cox.net>
2020-05-04 13:57:43 -0700

    fixed layout issue with Photo Detail component. Still need to add the other containers that use Photo Detail.

--- src/components/PhotoWithDetail.js
+++ src/components/PhotoWithDetail.js
@@ -1,43 +1,30 @@
 import React from "react";
 
 const PhotoWithDetail = props => {
+    // if a URL is is provided, hyperlink the title else just show the title
 
     return (
         <React.Fragment>
-        <div className="photo-detail">
-        <p><img src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`} className={props.class_name} alt={props.alt_text + props.title}/>
+            <div>
+                <div className="outer-container">
 
-        {props.link ? <a href={window.location.origin + "/" + props.link}><strong>{props.title}</strong></a>
-            : <strong>{props.title}</strong>}&mdash;{props.description}</p>
-        </div>
+                    <p className={props.container_class_name}><img
+                        src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`}
+                        className={props.image_class_name} alt={props.alt_text + props.title}/> {props.link ?
+                        <a href={window.location.origin + "/" + props.link}><strong>{props.title}</strong></a> :
+                        <strong>{props.title}</strong>}&mdash;{props.description}</p>
 
-          <hr/>
+                </div>
+
+                <hr/>
+
+            </div>
         </React.Fragment>
     )
 }
 
 export default PhotoWithDetail
 
-{/*if a URL is is provided, hyperlink the title else just show the title*/}
 
-{/* <div> */}
-{/* <div className="" style={{ display: "flex"}}>*/}
-{/**/}
-{/*      <div style={{flex: 1, margin: "0 10px 5px"}}>*/}
-{/*          <img src={`${window.location.origin}/assets/images/${props.image_folder}/${props.image_name}`}*/}
-{/*               className={props.class_name}*/}
-{/*               alt={props.alt_text + props.title}/>*/}
-{/*      </div>*/}
-{/**/}
-{/*      <div style={{flexGrow: 2, textAlign: "justify"}}>*/}
-{/*             /!* if a URL is is provided, hyperlink the title else just show the title *!/*/}
-{/*             {props.link ? <a href={window.location.origin + "/" + props.link}><strong>{props.title}</strong></a>*/}
-{/*                 : <strong>{props.title}</strong>}*/}
-{/*             &mdash;{props.description}*/}
-{/*     </div>*/}
-{/**/}
-{/**/}
-{/* </div>*/}
-{/**/}
-{/*<hr/>*/}
-{/*}</div>*/}
+
+
--- src/containers/Library.js
+++ src/containers/Library.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import PhotoWithDetail from "../components/PhotoWithDetail";
 
+import $ from "jquery";
+
 function assembleData() {
     let data = require("../assets/data/library.json");
 
@@ -12,7 +14,6 @@ function assembleData() {
     return libraryArray;
 }
 
-
 class Library extends Component {
 
     constructor(props) {
@@ -23,7 +24,9 @@ class Library extends Component {
         }
     }
 
-
+    componentDidMount() {
+        adjustSpacing();
+    }
 
     render () {
         return (
@@ -38,7 +41,8 @@ class Library extends Component {
                                                                          description={item.description}
                                                                          image_name={item.image_name}
                                                                          image_folder="library"
-                                                                         class_name="book-img"
+                                                                         container_class_name="library-detail"
+                                                                         image_class_name="book-img"
                                                                          alt_text="Book cover of: "/>)}
                 </div>
 
@@ -46,5 +50,24 @@ class Library extends Component {
         )
     }
 }
+
+//Must use window-load instead of document-ready to ensure images are loaded
+$(window).on("load resize", adjustSpacing);
+
+function adjustSpacing() {
+    $('.library-detail').each(function() {
+        let $this = $(this);
+
+        let maxHeight = parseInt($this.children('img').css("max-height").replace(/[^-\d.]/g, ''));
+        let imgHeight = $this.children('img').outerHeight();
+        let imgHeight2Use = (imgHeight > maxHeight) ? maxHeight : imgHeight;
+
+        let textHeight = $this[0].scrollHeight;
+        let divHeight = (imgHeight2Use > textHeight) ? imgHeight2Use + 5 : textHeight + 5;
+
+        $this.parent().css({"height": divHeight});
+    });
+}
+
 
 export default Library;
